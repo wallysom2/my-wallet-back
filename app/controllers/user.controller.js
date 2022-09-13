0bb284a98e2f8d59d02687f49a6d56e0aec17c97
Matheus <wallysom2@gmail.com>
2022-09-13 14:09:24 -0300

    feat user controller login

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -22,3 +22,35 @@ export async function signUp(req, res) {
   }
 
 }
+
+export async function login (req, res) {
+    try {
+      const user = await db.collection("users").findOne({email: req.body.email});
+      if(!user) return res.sendStatus(404);
+  
+      if(user && bcrypt.compareSync(req.body.password, user.password)) {
+        const token = uuid();
+        await db.collection("sessions").insertOne({token, userId: user._id});
+        return res.send({token, name: user.name});
+      }
+  
+      return res.sendStatus(404);
+    } catch (error) {
+      console.log(error);
+      return res.sendStatus(500);
+    }
+  }
+  
+  export async function logout(req, res) {
+    const {authorization} = req.headers;
+    const token = authorization?.replace("Bearer", "").trim();
+    if(!token) return res.send(403);
+    
+    try {
+      await db.collection("sessions").deleteOne({token});
+      res.sendStatus(200);
+    } catch (error) {
+      console.log("Nao foi possivel realizar o logout", error);
+      return res.sendStatus(500);
+    }
+}
