949fc3227abed36ad7a5649cb945e5a7ba4b650c
Matheus <wallysom2@gmail.com>
2022-09-13 14:13:53 -0300

    feat usercontroller logout

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,27 +1,6 @@
-import joi from "joi";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
-
 import db from "../database/db";
-
-export async function signUp(req, res) {
-  try {
-    const passwordHash = bcrypt.hashSync(req.body.password, SALT);
-    const SALT = 10;
-    
-    await db.collection("users").insertOne({
-      name: req.body.name,
-      email: req.body.email,
-      password: passwordHash
-    });
-
-    return res.status(201).send ("Usuario criado"); 
-  } catch (error) {
-    console.log("Erro ao criar usuario", error);
-    return res.sendStatus(500);
-  }
-
-}
 
 export async function login (req, res) {
     try {
@@ -37,6 +16,24 @@ export async function login (req, res) {
       return res.sendStatus(404);
     } catch (error) {
       console.log(error);
+      return res.sendStatus(500);
+    }
+  }
+
+  export async function register (req, res) {
+    try {
+      const passwordHash = bcrypt.hashSync(req.body.password, SALT);
+      const SALT = 10;
+      
+      await db.collection("users").insertOne({
+        name: req.body.name,
+        email: req.body.email,
+        password: passwordHash
+      });
+  
+      return res.status(201).send ("Usuario criado"); 
+    } catch (error) {
+      console.log("Erro ao criar usuario", error);
       return res.sendStatus(500);
     }
   }
